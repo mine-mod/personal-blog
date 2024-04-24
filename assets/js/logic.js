1d73accc13c94a3745fa6e9a1e315ef29fdbbad9
minemod <yolo1234100@hotmail.com>
2024-04-24 19:19:54 -0300

    added a nav bar and cleaned up the home page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,7 +3,3 @@ themeToggle.addEventListener('change', function() {
     document.body.classList.toggle('dark-mode');
 });
 
-
-document.getElementById('backButton').addEventListener('click', function() {
-    window.location.href = 'index.html';
-});
